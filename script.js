4e85f5a721b2feba41b1077d8a13121a4aa62138
GUSTAVO REBELATTO ZAPELLA <gustavo_zapella@estudante.sesisenai.org.br>
2022-08-17 15:07:27 -0300

    Counting system already done

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ var game = document.getElementById("game");
 var character = document.getElementById("character");
 var jumping = 0;
 var counter = 0;
+var counter2 = 0;
+var realCounter = 0;
 
 game.addEventListener('animationiteration', () => {
     block1.style.height = "35%";
@@ -11,10 +13,16 @@ game.addEventListener('animationiteration', () => {
     var random1 = Math.floor(Math.random() * 70);
     var random2 = (70 - random1);
     setHeight(random1, random2);
-    counter++;
+    if (counter > 0) {
+        realCounter = realCounter + ((counter2 + 1) - counter);
+        counter2 = 0;
+        counter = 0;
+    }
+    var counterHtml = document.getElementById("counter");
+    counterHtml.innerText = realCounter;
 });
 
-var rangeIntersect = function(min0, max0, min1, max1) {
+var rangeIntersect = function (min0, max0, min1, max1) {
     return Math.max(min0, max0) >= Math.min(min1, max1) && Math.min(min0, max0) <= Math.max(min1, max1)
 }
 var rectIntersect = function (r0, r1) {
@@ -23,11 +31,7 @@ var rectIntersect = function (r0, r1) {
 
 var interval = setInterval(() => {
     var characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
-
-    
     character.style.top = (characterTop + 3) + "px";
-
-    var alreadyJumped = 0;
 
     var boxBlock1 = document.getElementById("block1").getBoundingClientRect();
     var boxCharacter = character.getBoundingClientRect();
@@ -36,15 +40,26 @@ var interval = setInterval(() => {
     var cTop = -(500 - characterTop);
     if ((characterTop > 160) || (rectIntersect(boxCharacter, boxBlock1)) || (rectIntersect(boxCharacter, boxBlock2))) {
         alert("You lose");
+        localStorage.setItem("score", realCounter);
+        realCounter = 0;
         character.style.top = "-40%"
     }
 
-},9);
+    var blockLeft = parseInt(window.getComputedStyle(block1).getPropertyValue("left"));
+
+    if (blockLeft <= 60) {
+        counter++;
+        counter2++;
+
+    }
+}, 9);
+
 
 function jump() {
+
     jumping = 1;
     let jumpCount = 0;
-    
+
     var jumpInterval =
         setInterval(() => {
             var characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
@@ -60,7 +75,7 @@ function jump() {
             }
         }, 8);
 
-        character.style.animation = "character1 0.2s";
+    character.style.animation = "character1 0.2s";
 }
 
 function setHeight(random1, random2) {
@@ -71,4 +86,4 @@ function setHeight(random1, random2) {
 
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
-  }
+}
